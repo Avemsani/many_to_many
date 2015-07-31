67970360ed23b794d84b8c942062395adad98b77
Harsha Murthy <harshamurthy2@gmail.com>
2015-07-31 11:27:22 -0500

    Initial commit

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,55 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find_by(id: params[:id])
+
+    # These two lines are doing the same thing
+    # @enrollments = Enrollment.where(student_id: @student.id)
+    # @enrollments = @student.enrollments
+
+    @courses = @student.courses
+  end
+
+  def new
+  end
+
+  def create
+    @student = Student.new
+    @student.name = params[:name]
+    @student.email = params[:email]
+
+    if @student.save
+      redirect_to "/students/#{ @student.id }"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @student = Student.find_by(id: params[:id])
+  end
+
+  def update
+    @student = Student.find_by(id: params[:id])
+    @student.name = params[:name]
+    @student.email = params[:email]
+
+    if @student.save
+      redirect_to "/students/#{ @student.id }"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @student = Student.find_by(id: params[:id])
+    @student.destroy
+
+
+    redirect_to "/students"
+  end
+end
